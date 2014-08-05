4a54f788baa31a9a97f10c35e3972db759f6913e
Eldar Gabdullin <eldargab@gmail.com>
2014-08-05 21:28:07 +0400

    move Promise to separate package/project

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://github.com/dar-clojure/async"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.analyzer.jvm "0.1.0-beta12"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.analyzer.jvm "0.1.0-beta12"]
+                 [dar/async.promise "0.1.0"]])
